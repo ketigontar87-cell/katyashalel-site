96876ccc3240876e4d48de7846f4b378b0f6d55d
ketigontar87-cell <ketigontar87@gmail.com>
2026-07-08 18:27:31 +0200

    add: named image endpoint img5

---
+++ api/img5.js
@@ -0,0 +1,9 @@
+export default async function handler(req, res) {
+  const r = await fetch("https://d8j0ntlcm91z4.cloudfront.net/user_3G7eLtlI2FRc56F1KISDHpMfrwV/hf_20260708_150452_0046fbf4-4ade-47a1-9589-ed409ce44afa.png");
+  if (!r.ok) { res.status(502).send("upstream"); return; }
+  const buf = Buffer.from(await r.arrayBuffer());
+  res.setHeader("Content-Type", "image/png");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Cache-Control", "public, max-age=604800");
+  res.status(200).send(buf);
+}
